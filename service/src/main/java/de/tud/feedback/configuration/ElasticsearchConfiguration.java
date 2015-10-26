93c8ed4a2ee9660b281e52e7f699f09e1a5d48d5
Peter Heisig <peter.heisig@medien-dresden.de>
2015-10-26 17:31:01 +0100

    Elasticsearch configuration from Spring Boot

--- service/src/main/java/de/tud/feedback/configuration/ElasticsearchConfiguration.java
+++ service/src/main/java/de/tud/feedback/configuration/ElasticsearchConfiguration.java
@@ -1,58 +1,12 @@
 package de.tud.feedback.configuration;
 
 import de.tud.feedback.repository.index.HistoricalStateRepository;
-import org.elasticsearch.client.Client;
-import org.elasticsearch.client.transport.TransportClient;
-import org.elasticsearch.common.transport.InetSocketTransportAddress;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.EnvironmentAware;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
-import org.springframework.core.env.Environment;
 import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
-
-import static org.elasticsearch.node.NodeBuilder.nodeBuilder;
 
 @Configuration
 @EnableElasticsearchRepositories(
         basePackageClasses = HistoricalStateRepository.class)
 public class ElasticsearchConfiguration {
-
-    static final Logger LOG = LoggerFactory.getLogger(ElasticsearchConfiguration.class);
-
-    @Configuration
-    @Profile("embeddedElasticsearch")
-    public static class EmbeddedElasticsearchConfiguration {
-
-        @Bean(name = "elasticsearchClient")
-        public Client embeddedClient() {
-            LOG.warn("Using embedded server");
-            return nodeBuilder().local(true).node().client();
-        }
-
-    }
-
-    @Configuration
-    @Profile("!embeddedElasticsearch")
-    public static class ExternalElasticsearchConfiguration implements EnvironmentAware {
-
-        @Bean(name = "elasticsearchClient")
-        public Client externalClient(
-                @Value("${service.knowledge.elasticsearch.host}") String host,
-                @Value("${service.knowledge.elasticsearch.port:0}") int port
-        ) {
-            return new TransportClient().addTransportAddress(new InetSocketTransportAddress(host, port));
-        }
-
-        @Override
-        public void setEnvironment(Environment env) {
-            env.getRequiredProperty("service.knowledge.elasticsearch.host");
-            env.getRequiredProperty("service.knowledge.elasticsearch.port");
-        }
-
-    }
 
 }
