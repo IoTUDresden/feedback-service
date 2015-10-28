ba4c13e5fa9b7f59c7f18ef96bc3480506ff901d
Peter Heisig <peter.heisig@medien-dresden.de>
2015-10-28 11:18:45 +0100

    Big refac. (cont.)

--- service/src/main/java/de/tud/feedback/FeedbackService.java
+++ service/src/main/java/de/tud/feedback/FeedbackService.java
@@ -1,13 +1,10 @@
 package de.tud.feedback;
 
-import de.tud.feedback.event.LoopEvent;
-import de.tud.feedback.event.SymptomDetectedEvent;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.EnvironmentAware;
-import org.springframework.context.event.EventListener;
 import org.springframework.core.env.Environment;
 
 import java.util.Arrays;
@@ -18,11 +15,6 @@ import static java.lang.String.format;
 public class FeedbackService implements EnvironmentAware {
 
     private static final Logger LOG = LoggerFactory.getLogger(FeedbackService.class);
-
-    @EventListener
-    public void log(LoopEvent event) {
-        if (!(event instanceof SymptomDetectedEvent)) LOG.info(event.toString());
-    }
 
     @Override
     public void setEnvironment(Environment environment) {
